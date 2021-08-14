3dac5ebf5246157fff4d5b2692df47731209030d
lay <1581133593@qq.com>
2021-08-14 20:32:57 +0800

    8/14

--- src/pages/discover/c-page/ranking/c-cpn/rank-list/index.js
+++ src/pages/discover/c-page/ranking/c-cpn/rank-list/index.js
@@ -1,5 +1,7 @@
 import React, { memo } from 'react'
 import { shallowEqual, useSelector } from 'react-redux'
+import { getSizeImage, formatMinuteSecond } from '@/utils/format-utils'
+
 
 import YQThemeHeaderSong from '@/components/theme-header-song'
 import { WrapperRankList } from './style'
@@ -9,7 +11,7 @@ export default memo(function YQRankList() {
   const state = useSelector(state => ({
     rankingList: state.getIn(["ranking", "rankingList"])
   }), shallowEqual)
-  const traks = state.rankingList.traks || []
+  const tracks = state.rankingList.tracks || []
 
   //const dispatch = useDispatch()
 
@@ -20,6 +22,45 @@ export default memo(function YQRankList() {
   return (
     <WrapperRankList>
       <YQThemeHeaderSong></YQThemeHeaderSong>
+      <div className="ranking-list">
+        <table>
+          <thead>
+            <tr className="header">
+              <th className="ranking"></th>
+              <th className="title">标题</th>
+              <th className="duration">时长</th>
+              <th className="singer">歌手</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              tracks.map((item, index) => {
+                return (
+                  <tr key={item.id}>
+                    <td>
+                      <div className="rank-num">
+                        <span className="num">{index + 1}</span>
+                        <span className="new sprite_icon2"></span>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="song-name">
+                        {
+                          index < 3 ? (<img src={getSizeImage(item.al.picUrl, 50)} alt="" />) : null
+                        }
+                        <span className="play sprite_table"></span>
+                        <span className="name">{item.name}</span>
+                      </div>
+                    </td>
+                    <td>{formatMinuteSecond(item.dt)}</td>
+                    <td>{item.ar[0].name}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
     </WrapperRankList>
   )
 })
--- src/pages/discover/c-page/ranking/c-cpn/rank-list/style.js
+++ src/pages/discover/c-page/ranking/c-cpn/rank-list/style.js
@@ -2,4 +2,89 @@ import styled from "styled-components";
 
 export const WrapperRankList = styled.div`
 padding: 0 30px 40px 40px;
+
+.ranking-list {
+  table {
+    width: 100%;
+    border: 1px solid #d9d9d9;
+    thead {
+      th {
+         height: 34px;
+          line-height: 34px;
+          background-image: url(${require("@/assets/img/sprite_table.png").default});
+          color: #666;
+          border: 1px solid #ddd;
+          border-width: 0 0 1px 1px;
+          padding-left: 10px;
+          box-shadow: 0px 1px 2px 0px rgba(0,0,0,0.25);
+         
+      }
+      .ranking {
+          width: 78px;
+          border-left: none;
+        }
+      .title {
+       
+      }
+        .duration {
+          width: 91px;
+        }
+
+        .singer {
+          width: 173px;
+        }
+    }
+     tbody {
+        td {
+          padding: 6px 10px;
+        }
+
+        tr:nth-child(2n) {
+          background-color: #fff;
+        }
+
+        tr:nth-child(2n+1) {
+          background-color: #f7f7f7;
+        }
+
+        .rank-num {
+          display: flex;
+
+          .num {
+            width: 25px;
+            height: 18px;
+            text-align: center;
+            color: #999;
+          }
+
+          .new {
+            width: 16px;
+            height: 17px;
+            margin-left: 12px;
+            background-position: -67px -283px;
+          }
+        }
+
+        .song-name {
+          display: flex;
+          align-items: center;
+          img {
+            width: 50px;
+            height: 50px;
+            margin-right: 10px;
+          }
+
+          .play {
+            width: 17px;
+            height: 17px;
+            background-position: 0 -103px;
+          }
+
+          .name {
+            margin-left: 10px;
+          }
+        }
+      }
+  }
+}
 `
