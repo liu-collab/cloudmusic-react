71200b6a3d7bf37e262e82e6f4f3e8da932650e1
lay <1581133593@qq.com>
2021-09-25 16:41:59 +0800

    9/25

--- src/pages/player/c-cpn/playinfo/style.js
+++ src/pages/player/c-cpn/playinfo/style.js
@@ -1,99 +1,100 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const WrapperPlayInfo = styled.div`
-display: flex;
-padding:47px 30px 40px 39px ;
-`
+  display: flex;
+  padding: 47px 30px 40px 39px;
+`;
 
 export const PlayInfoLeft = styled.div`
-width: 206px;
+  width: 206px;
 
-.image{
-  position: relative;
-  display: flex;
-  width: 198px;
-  height: 198px;
-  justify-content: center;
-  align-items: center;
-  img {
+  .image {
     position: relative;
-    top: -2px;
-    left: 4px;
-  }
+    display: flex;
+    width: 198px;
+    height: 198px;
+    justify-content: center;
+    align-items: center;
+    img {
+      position: relative;
+      top: -2px;
+      left: 4px;
+    }
 
-  .cover{
-     background-position: -140px -580px;
-     right: -6px;
-     top: -6px;
-     width: 206px;
-     height: 205px;
+    .cover {
+      background-position: -140px -580px;
+      right: -6px;
+      top: -6px;
+      width: 206px;
+      height: 205px;
+    }
   }
-}
 
   .link {
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   margin: 20px;
-   i {
-     display: inline-block;
-     width: 16px;
-     height: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px;
+    i {
+      display: inline-block;
+      width: 16px;
+      height: 16px;
       background-position: -34px -863px;
-   }
-   a {
-     color:  #0c73c2;
-     text-decoration: underline;
-   }
+    }
+    a {
+      color: #0c73c2;
+      text-decoration: underline;
+    }
   }
- 
-`
+`;
 
 export const PlayInfoRight = styled.div`
-width: 414px;
-margin-left: 20px;
+  width: 414px;
+  margin-left: 20px;
 
-.header{
-  display: flex;
-  align-items: center;
-  i {
-     display: inline-block;
+  .header {
+    display: flex;
+    align-items: center;
+    i {
+      display: inline-block;
       width: 54px;
       height: 24px;
       background-position: 0 -463px;
+    }
+    .title {
+      margin-left: 10px;
+      font-size: 24px;
+      font-weight: 400;
+    }
   }
-  .title {
-    margin-left: 10px;
-    font-size: 24px;
-    font-weight: 400;
-  }
-}
 
-.singer , .album {
-  margin: 10px;
-  a {
+  .singer,
+  .album {
+    margin: 10px;
+    a {
       margin-left: 5px;
       color: #0c73c2;
+    }
   }
-}
-.Lyric {
-  margin: 13px 0 0;
-  .text{
-    margin-top: 5px;
+  .Lyric {
+    margin: 13px 0 0;
+    .text {
+      margin-top: 5px;
+    }
   }
-}
-.lyric-control{
-  position: relative;
-  text-decoration: underline;
-  color: #0c73c2;
-  background-color: #fff;
-  i{
+  .lyric-control {
+    position: relative;
+    text-decoration: underline;
+    color: #0c73c2;
+    background-color: #fff;
+    cursor: pointer;
+    i {
       position: absolute;
       width: 11px;
       height: 8px;
       right: -8px;
       top: 5px;
-      background-position: ${props => props.isSpread ? "-45px" : "-65px"} -520px;
+      background-position: ${(props) => (props.isSpread ? '-45px' : '-65px')} -520px;
+    }
   }
-}
-`
+`;
--- src/pages/player/c-cpn/simiSong/index.js
+++ src/pages/player/c-cpn/simiSong/index.js
@@ -1,50 +1,51 @@
-import React, { memo } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getSimiSongAction } from '../../store/actionsCreators'
-import { WrapperSimiSong } from './style'
-import YQThemeHeaderPlay from '@/components/theme-header-play'
-import { useEffect } from 'react'
+import React, { memo } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { getSimiSongAction } from '../../store/actionsCreators';
+import { WrapperSimiSong } from './style';
+import YQThemeHeaderPlay from '@/components/theme-header-play';
+import { useEffect } from 'react';
 
 export default memo(function YQSimiSong() {
-
   //redux hooks
-  const dispatch = useDispatch()
-  const { simiSongs } = useSelector(state => ({
-    simiSongs: state.getIn(["player", "simiSongs"])
-  }))
+  const dispatch = useDispatch();
+  const { simiSongs } = useSelector(
+    (state) => ({
+      simiSongs: state.getIn(['player', 'simiSongs']),
+    }),
+    shallowEqual
+  );
   //other Hooks
   useEffect(() => {
-    dispatch(getSimiSongAction())
-  })
+    dispatch(getSimiSongAction());
+  }, [dispatch]);
 
   //const artist = simiSongs && simiSongs.artists && simiSongs.artists[0] && simiSongs.artists[0].name
-  const artist = ""
+  const artist = '';
   return (
     <WrapperSimiSong>
       <YQThemeHeaderPlay title="相似歌曲"></YQThemeHeaderPlay>
       <div className="songs">
-
-        {
-          simiSongs.map((item, index) => {
-            return (
-              <div className="song-item" key={item.id}>
-                <div className="info">
-                  <div className="title">
-                    <a href="#/">{item.name}</a>
-                  </div>
-                  <div className="artist">
-                    <a href="#/">{artist ? "simiSongs.artists[0].name" : "未知歌手"}</a>
-                  </div>
+        {simiSongs.map((item, index) => {
+          return (
+            <div className="song-item" key={item.id}>
+              <div className="info">
+                <div className="title">
+                  <a href="#/">{item.name}</a>
                 </div>
-                <div className="operate">
-                  <button className="item sprite_icon3 play"></button>
-                  <button className="item sprite_icon3 add"></button>
+                <div className="artist">
+                  <a href="#/">
+                    {artist ? 'simiSongs.artists[0].name' : '未知歌手'}
+                  </a>
                 </div>
               </div>
-            )
-          })
-        }
+              <div className="operate">
+                <button className="item sprite_icon3 play"></button>
+                <button className="item sprite_icon3 add"></button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </WrapperSimiSong>
-  )
-})
+  );
+});
