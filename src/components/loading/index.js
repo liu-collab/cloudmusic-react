ba9c65e7e4ee213cbf4e9609eb816ebc8fabc090
lay <1581133593@qq.com>
2021-10-29 15:54:57 +0800

    10/29

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -5,7 +5,7 @@ import { WrapperLoading } from './style';
 export default memo(function YQIsLoading() {
   return (
     <WrapperLoading className="wrap-v2">
-      <Spin tip="Loading..." size="large">
+      <Spin tip="isLoading..." size="large">
         <Alert message="加载中" type="info" />
       </Spin>
     </WrapperLoading>
